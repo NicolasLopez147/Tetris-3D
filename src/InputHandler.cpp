236289d998626c03481ac0ff5bfb8687e1261983
nilopezn <nilopezn@unal.edu.co>
2025-01-21 13:28:48 +0100

    Update InputHandler.cpp

--- src/InputHandler.cpp
+++ src/InputHandler.cpp
@@ -1,27 +1,37 @@
-
 #include "Game.cpp"
+
 class InputHandler {
-  public:
-    void handleInput(int key, Game& game){
-      switch(key){
-        case 's':
-          game.moveTetromino(Vector3D(0,-1,0));
-          break;
-        case 'a':
-          game.moveTetromino(Vector3D(-1,0,0));
-          break;
-        case 'd':
-          game.moveTetromino(Vector3D(1,0,0));
-          break;
-        case 'w':
-          game.rotateTetromino(90,Vector3D(0,0,1));
-          break;
-        case 'q':
-          game.rotateTetromino(90,Vector3D(0,1,0));
-          break;
-        case 'e':
-          game.rotateTetromino(90,Vector3D(1,0,0));
-          break;
-      }
+public:
+    // Processes user input and performs the corresponding actions on the game
+    void handleInput(int key, Game& game) {
+        switch (key) {
+        case 's': // Move the current Tetromino down
+            game.moveTetromino(Vector3D(0, -1, 0));
+            break;
+
+        case 'a': // Move the current Tetromino left
+            game.moveTetromino(Vector3D(-1, 0, 0));
+            break;
+
+        case 'd': // Move the current Tetromino right
+            game.moveTetromino(Vector3D(1, 0, 0));
+            break;
+
+        case 'w': // Rotate the Tetromino 90 degrees around the Z-axis
+            game.rotateTetromino(90, Vector3D(0, 0, 1));
+            break;
+
+        case 'q': // Rotate the Tetromino 90 degrees around the Y-axis
+            game.rotateTetromino(90, Vector3D(0, 1, 0));
+            break;
+
+        case 'e': // Rotate the Tetromino 90 degrees around the X-axis
+            game.rotateTetromino(90, Vector3D(1, 0, 0));
+            break;
+
+        default:
+            // Optional: Handle invalid keys or no-op
+            break;
+        }
     }
 };
