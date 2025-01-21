5656421dd11beb4d40163b8b7febd5bdc153333b
nilopezn <nilopezn@unal.edu.co>
2025-01-21 13:28:11 +0100

    Update Block.cpp

--- src/Block.cpp
+++ src/Block.cpp
@@ -1,17 +1,34 @@
 #include <iostream>
 #include "Vector.cpp"
+
 class Block {
 private:
+    // Attribute to store the block's position in 3D space
     Vector3D position;
+
+    // Attribute to store the block's color, represented as an RGB vector
     Vector3D color;
+
 public:
+    // Constructor: Initializes a block with a given position and color
     Block(const Vector3D& pos, const Vector3D& col) : position(pos), color(col) {}
+
+    // Method to draw the block
+    // Outputs the block's position and color to the console
     void draw() const {
-        std::cout << "Drawing block at position (" << position.x << ", " << position.y << ", " << position.z << ") with color (" << color.x << ", " << color.y << ", " << color.z << ")" << std::endl;
+        std::cout << "Drawing block at position (" << position.x << ", " << position.y << ", " << position.z 
+                  << ") with color (" << color.x << ", " << color.y << ", " << color.z << ")" << std::endl;
     }
+
+    // Method to set a new position for the block
     void setPosition(const Vector3D& newPos) { position = newPos; }
+
+    // Method to retrieve the current position of the block
     Vector3D getPosition() const { return position; }
 
+    // Method to set a new color for the block
     void setColor(const Vector3D& newColor) { color = newColor; }
+
+    // Method to retrieve the current color of the block
     Vector3D getColor() const { return color; }
 };
