b5276902d3e0a22301c1d26a01f0abbe6015c5ef
Nicolas <nilopezn@unal.edu.co>
2025-01-26 12:12:10 +0100

    Created classes

--- main.cpp
+++ main.cpp
@@ -53,128 +53,247 @@ void main() {
 }
 )";
 
+class Shader{
+    public:
+        GLuint ID;
 
-// Compile and link the vertex and fragment shaders into a shader program
-GLuint createShaderProgram(const char* vertexSrc, const char* fragmentSrc) {
-    // Compile the vertex shader
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexSrc, nullptr);
-    glCompileShader(vertexShader);
+        Shader(const char* vertexSource , const char*fragmentSource){
+            ID = createShaderProgram(vertexSource, fragmentSource);
+        }
+        GLuint getShaderID(){
+            return ID;
+        }
 
-    // Compile the fragment shader
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentSrc, nullptr);
-    glCompileShader(fragmentShader);
+        GLuint createShaderProgram(const char* vertexSrc, const char* fragmentSrc) {
+            // Compile the vertex shader
+            GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+            glShaderSource(vertexShader, 1, &vertexSrc, nullptr);
+            glCompileShader(vertexShader);
 
-    // Link the shaders into a shader program
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
+            // Compile the fragment shader
+            GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+            glShaderSource(fragmentShader, 1, &fragmentSrc, nullptr);
+            glCompileShader(fragmentShader);
 
-    // Clean up the individual shaders as they are no longer needed
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+            // Link the shaders into a shader program
+            GLuint shaderProgram = glCreateProgram();
+            glAttachShader(shaderProgram, vertexShader);
+            glAttachShader(shaderProgram, fragmentShader);
+            glLinkProgram(shaderProgram);
 
-    return shaderProgram;
-}
+            // Clean up the individual shaders as they are no longer needed
+            glDeleteShader(vertexShader);
+            glDeleteShader(fragmentShader);
 
-// Generate vertices for a 3D grid based on width, height, and depth
-std::vector<float> generateGridVertices(int width, int height, int depth) {
-    std::vector<float> vertices;
+            return shaderProgram;
+        }
 
-    for (int y = 0; y <= height; ++y) {
-        // Líneas horizontales
-        vertices.insert(vertices.end(), { 0, (float)y, 0, (float)width, (float)y, 0 });
-        vertices.insert(vertices.end(), { 0, (float)y, 0, 0, (float)y, (float)depth });
-    }
+        void use() {
+            glUseProgram(ID);
+        }
 
-    for (int z = 0; z <= depth; ++z) {
-        // Líneas verticales
-        vertices.insert(vertices.end(), { 0, 0, (float)z, 0, (float)height, (float)z });
-        vertices.insert(vertices.end(), { 0, 0, (float)z, (float)width, 0, (float)z });
-    }
+        void setUniformMatrix4fv(const std::string& name, glm::mat4 matrix) const {
+            GLuint loc = glGetUniformLocation(ID, name.c_str());
+            glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(matrix));
+        }
 
-    for (int x = 0; x <= width; ++x) {
-        vertices.insert(vertices.end(), { (float)x, 0, 0, (float)x, (float)height, 0 });
-        vertices.insert(vertices.end(), { (float)x, 0, 0, (float)x, 0, (float)depth });
-    }
+        void setUniform3f(const std::string& name, float x, float y, float z) const {
+            GLuint loc = glGetUniformLocation(ID, name.c_str());
+            glUniform3f(loc, x, y, z);
+        }
 
-    return vertices;
-}
+        void setUniform1i(const std::string& name, int value) const {
+            GLuint loc = glGetUniformLocation(ID, name.c_str());
+            glUniform1i(loc, value);
+        }
 
-
-std::vector<float> cubeVertices = {
-    // Posiciones de los vértices
-    0.0f, 0.0f, 0.0f,  // 0: Bottom-left-back
-     1.0f, 0.0f, 0.0f,  // 1: Bottom-right-back
-     1.0f,  1.0f, 0.0f,  // 2: Top-right-back
-    0.0f,  1.0f, 0.0f,  // 3: Top-left-back
-    0.0f, 0.0f,  1.0f,  // 4: Bottom-left-front
-     1.0f, 0.0f,  1.0f,  // 5: Bottom-right-front
-     1.0f,  1.0f,  1.0f,  // 6: Top-right-front
-    0.0f,  1.0f,  1.0f   // 7: Top-left-front
-};
-
-std::vector<unsigned int> cubeIndices = {
-    // Back face
-    0, 1, 2,
-    2, 3, 0,
-    // Front face
-    4, 5, 6,
-    6, 7, 4,
-    // Left face
-    0, 4, 7,
-    7, 3, 0,
-    // Right face
-    1, 5, 6,
-    6, 2, 1,
-    // Bottom face
-    0, 1, 5,
-    5, 4, 0,
-    // Top face
-    3, 2, 6,
-    6, 7, 3
 };
 
 
-// Process keyboard inputs to move the cube
-void processInput(GLFWwindow* window, glm::vec3& cubePosition) {
-    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-        cubePosition.x -= 0.1f;  // Move left
-    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-        cubePosition.x += 0.1f;  // Move right
-    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-        cubePosition.y += 0.1f;  // Move up
-    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-        cubePosition.y -= 0.1f;  // Move down
-}
 
-GLuint createVAO(const std::vector<float>& vertices, int vertexSize) {
-    GLuint VAO, VBO , EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
+class Grid{
+    GLuint VAO;
+    int vertexCount;
 
-    glBindVertexArray(VAO);
+    public:
+        Grid(int width, int height, int depth){
+            std::vector<float> vertices = generateGridVertices(width, height, depth);
+            vertexCount = vertices.size() / 3;
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+            glGenVertexArrays(1, &VAO);
+            glBindVertexArray(VAO);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, cubeIndices.size() * sizeof(unsigned int), cubeIndices.data(), GL_STATIC_DRAW);
+            GLuint VBO;
+            glGenBuffers(1, &VBO);
+            glBindBuffer(GL_ARRAY_BUFFER, VBO);
+            glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+            glEnableVertexAttribArray(0);
 
+            glBindBuffer(GL_ARRAY_BUFFER, 0);
+            glBindVertexArray(0);
+        }
 
-    glVertexAttribPointer(0, vertexSize, GL_FLOAT, GL_FALSE, vertexSize * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+        void draw(const Shader& shader) const {
+            shader.setUniform1i("isGRID", true);
+            glm::mat4 identityModel = glm::mat4(1.0f);
+            shader.setUniformMatrix4fv("model", identityModel);
+            glBindVertexArray(VAO);
+            glDrawArrays(GL_LINES, 0, vertexCount);
+            glBindVertexArray(0);
+        }
+    private:
+        // Generate vertices for a 3D grid based on width, height, and depth
+        std::vector<float> generateGridVertices(int width, int height, int depth) {
+            std::vector<float> vertices;
 
+            for (int y = 0; y <= height; ++y) {
+                // Líneas horizontales
+                vertices.insert(vertices.end(), { 0, (float)y, 0, (float)width, (float)y, 0 });
+                vertices.insert(vertices.end(), { 0, (float)y, 0, 0, (float)y, (float)depth });
+            }
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
+            for (int z = 0; z <= depth; ++z) {
+                // Líneas verticales
+                vertices.insert(vertices.end(), { 0, 0, (float)z, 0, (float)height, (float)z });
+                vertices.insert(vertices.end(), { 0, 0, (float)z, (float)width, 0, (float)z });
+            }
 
-    return VAO;
-}
+            for (int x = 0; x <= width; ++x) {
+                vertices.insert(vertices.end(), { (float)x, 0, 0, (float)x, (float)height, 0 });
+                vertices.insert(vertices.end(), { (float)x, 0, 0, (float)x, 0, (float)depth });
+            }
 
+            return vertices;
+        }
+
+};
+
+class Cube{
+    private:
+        GLuint VAO, EBO;
+        glm::vec3 position;
+        glm::vec3 color;
+        std::vector<float> cubeVertices = {
+            // Posiciones de los vértices
+            0.0f, 0.0f, 0.0f,  // 0: Bottom-left-back
+            1.0f, 0.0f, 0.0f,  // 1: Bottom-right-back
+            1.0f,  1.0f, 0.0f,  // 2: Top-right-back
+            0.0f,  1.0f, 0.0f,  // 3: Top-left-back
+            0.0f, 0.0f,  1.0f,  // 4: Bottom-left-front
+            1.0f, 0.0f,  1.0f,  // 5: Bottom-right-front
+            1.0f,  1.0f,  1.0f,  // 6: Top-right-front
+            0.0f,  1.0f,  1.0f   // 7: Top-left-front
+        };
+
+        std::vector<unsigned int> cubeIndices = {
+            // Back face
+            0, 1, 2,
+            2, 3, 0,
+            // Front face
+            4, 5, 6,
+            6, 7, 4,
+            // Left face
+            0, 4, 7,
+            7, 3, 0,
+            // Right face
+            1, 5, 6,
+            6, 2, 1,
+            // Bottom face
+            0, 1, 5,
+            5, 4, 0,
+            // Top face
+            3, 2, 6,
+            6, 7, 3
+        };
+
+    public:
+        Cube(glm::vec3 position, glm::vec3 color): position(position), color(color){
+            
+            GLuint VBO;
+
+            glGenVertexArrays(1, &VAO);
+            glGenBuffers(1, &VBO);
+            glGenBuffers(1, &EBO);
+
+            glBindVertexArray(VAO);
+
+            glBindBuffer(GL_ARRAY_BUFFER, VBO);
+            glBufferData(GL_ARRAY_BUFFER, cubeVertices.size() * sizeof(float), cubeVertices.data(), GL_STATIC_DRAW);
+
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, cubeIndices.size() * sizeof(unsigned int), cubeIndices.data(), GL_STATIC_DRAW);
+
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+            glEnableVertexAttribArray(0);
+
+            glBindBuffer(GL_ARRAY_BUFFER, 0);
+            glBindVertexArray(0);
+        }
+
+        void draw(const Shader& shader){
+            shader.setUniform3f("cubeColor", color.x, color.y, color.z);
+            shader.setUniform1i("isGRID", false);
+
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), position);
+            shader.setUniformMatrix4fv("model", model);
+            glBindVertexArray(VAO);
+            glDrawElements(GL_TRIANGLES, cubeIndices.size(), GL_UNSIGNED_INT, 0);
+            glBindVertexArray(0);
+        }
+
+        void setPosition(glm::vec3 newPosition){
+            position = newPosition;
+        }
+};
+
+class Render{
+    Shader shader;
+    Grid grid;
+    std::vector<Cube> cubes;
+    public:
+        Render(const Shader& shader, const Grid& grid): shader(shader), grid(grid){}
+
+        void addCube(const Cube& cube){
+            cubes.push_back(cube);
+        }
+
+        void render(const glm::mat4& projection, const glm::mat4& view){
+            shader.use();
+            shader.setUniformMatrix4fv("projection", projection);
+            shader.setUniformMatrix4fv("view", view);
+
+            grid.draw(shader);
+            for (Cube& cube : cubes){
+                cube.draw(shader);
+            }
+        }
+};
+
+class Game{
+    private:
+        GLFWwindow* window;
+        Render renderer;
+    public:
+        Game(GLFWwindow* window, Render renderer): window(window), renderer(renderer){}
+
+        void run(){
+            glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)1600 / 1200, 0.1f, 100.0f);
+            glm::mat4 view = glm::lookAt(glm::vec3(15, 25, 15), glm::vec3(5, 10, 5), glm::vec3(0, 1, 0));
+
+            while (!glfwWindowShouldClose(window)) {
+                // Limpiar la pantalla
+                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+                glEnable(GL_DEPTH_TEST);
+                
+                renderer.render(projection, view);
+
+                // Intercambiar buffers y procesar eventos
+                glfwSwapBuffers(window);
+                glfwPollEvents();
+            }
+        }
+};
 
 int main() {
     // Initialize GLFW
@@ -216,85 +335,17 @@ int main() {
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     // Create the shader program
-    GLuint shaderProgram = createShaderProgram(vertexShaderSource, fragmentShaderSource);
+    Shader shaderProgram(vertexShaderSource, fragmentShaderSource);
 
     // Generate grid vertices and set up a VAO/VBO for the grid
-    std::vector<float> gridVertices = generateGridVertices(6, 15, 6);
-    GLuint gridVAO = createVAO(gridVertices, 3);
+    Grid grid(6, 15, 6);
+    Render render(shaderProgram, grid);
+    render.addCube(Cube(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f, 0.0f, 0.0f)));
+    Cube cube(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f, 0.0f, 1.0f));
+    cube.setPosition(glm::vec3(0.0f, 10.0f, 1.0f));
+    render.addCube(cube);
 
-
-    // Generate cube vertices and set up a VAO/VBO for the cube
-    glm::vec3 cube1Position(0.0f, 0.0f, 0.0f);
-    glm::vec3 cube2Position(1.0f, 0.0f, 0.0f);
-
-    glm::vec3 targetOffset(0.0f,0.0f,0.0f);
-    float cubeSize = 1.0f;
-    // std::vector<float> cubeVertices = generateCubeVertices(0.0f,0.0f,1.0f, cubeSize);
-    GLuint cubeVAO = createVAO(cubeVertices, 3);
-
-    // Set up projection and view matrices
-    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)viewportWidth / viewportHeight, 0.1f, 100.0f);
-    glm::mat4 view = glm::lookAt(glm::vec3(15, 25, 15), glm::vec3(5, 10, 5), glm::vec3(0, 1, 0));
-
-    // Render loop
-    while (!glfwWindowShouldClose(window)) {
-
-        // Calcular las nuevas posiciones interpoladas
-        glm::vec3 cube1NewPosition = cube1Position + targetOffset;
-        glm::vec3 cube2NewPosition = cube2Position + targetOffset;
-
-        // Limpiar la pantalla
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glEnable(GL_DEPTH_TEST);
-        glUseProgram(shaderProgram);
-
-        // Enviar matrices de proyección y vista al shader
-        GLuint projectionLoc = glGetUniformLocation(shaderProgram, "projection");
-        GLuint viewLoc = glGetUniformLocation(shaderProgram, "view");
-        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-
-        
-        // Dibujar el cubo 1
-        GLuint isGRIDLoc = glGetUniformLocation(shaderProgram, "isGRID");
-        GLuint cubeColorLoc = glGetUniformLocation(shaderProgram, "cubeColor");
-        GLuint modelLoc = glGetUniformLocation(shaderProgram, "model");
-        glUniform1i(isGRIDLoc, false);
-        glUniform3f(cubeColorLoc, 1.0f, 0.0f, 0.0f);
-        glm::mat4 model1 = glm::translate(glm::mat4(1.0f), cube1NewPosition);
-        glBindVertexArray(cubeVAO);
-        glDrawElements(GL_TRIANGLES, cubeIndices.size(), GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
-
-        // Block block1(Vector3D(cube1NewPosition.x, cube1NewPosition.y, cube1NewPosition.z), Vector3D(1.0f, 0.0f, 0.0f));
-
-
-        // Dibujar el cubo 2
-        glUniform1i(isGRIDLoc, false);
-        glUniform3f(cubeColorLoc, 0.0f, 1.0f, 0.0f);
-        glm::mat4 model2 = glm::translate(glm::mat4(1.0f), cube2NewPosition);
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model2));
-        glBindVertexArray(cubeVAO);
-        glDrawElements(GL_TRIANGLES, cubeIndices.size(), GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
-
-
-        glm::mat4 identityModel = glm::mat4(1.0f);
-        glUniform1i(isGRIDLoc, true);
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(identityModel));
-        glBindVertexArray(gridVAO);
-        glDrawArrays(GL_LINES, 0, gridVertices.size() / 3);
-        glBindVertexArray(0);
-
-        // Intercambiar buffers y procesar eventos
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    // Limpieza de recursos
-    glDeleteVertexArrays(1, &cubeVAO);
-    glDeleteProgram(shaderProgram);
-    glfwDestroyWindow(window);
-    glfwTerminate();
+    Game game(window, render);
+    game.run();
     return 0;
 }
--- src/Block.cpp
+++ src/Block.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include "Vector.cpp"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class Block {
 private:
@@ -10,14 +15,54 @@ private:
     Vector3D color;
 
 public:
+
+    std::vector<float> cubeVertices = {
+        // Posiciones de los vértices
+        0.0f, 0.0f, 0.0f,  // 0: Bottom-left-back
+        1.0f, 0.0f, 0.0f,  // 1: Bottom-right-back
+        1.0f,  1.0f, 0.0f,  // 2: Top-right-back
+        0.0f,  1.0f, 0.0f,  // 3: Top-left-back
+        0.0f, 0.0f,  1.0f,  // 4: Bottom-left-front
+        1.0f, 0.0f,  1.0f,  // 5: Bottom-right-front
+        1.0f,  1.0f,  1.0f,  // 6: Top-right-front
+        0.0f,  1.0f,  1.0f   // 7: Top-left-front
+    };
+
+    std::vector<unsigned int> cubeIndices = {
+        // Back face
+        0, 1, 2,
+        2, 3, 0,
+        // Front face
+        4, 5, 6,
+        6, 7, 4,
+        // Left face
+        0, 4, 7,
+        7, 3, 0,
+        // Right face
+        1, 5, 6,
+        6, 2, 1,
+        // Bottom face
+        0, 1, 5,
+        5, 4, 0,
+        // Top face
+        3, 2, 6,
+        6, 7, 3
+    };
+    
     // Constructor: Initializes a block with a given position and color
     Block(const Vector3D& pos, const Vector3D& col) : position(pos), color(col) {}
 
     // Method to draw the block
-    // Outputs the block's position and color to the console
-    void draw() const {
-        std::cout << "Drawing block at position (" << position.x << ", " << position.y << ", " << position.z 
-                  << ") with color (" << color.x << ", " << color.y << ", " << color.z << ")" << std::endl;
+    void draw()  {
+        // Set the color of the block
+        glColor3f(color.x, color.y, color.z);
+
+        // Draw the block as a cube
+        glBegin(GL_TRIANGLES);
+        for (int i = 0; i < cubeIndices.size(); i++) {
+            glVertex3f(cubeVertices[cubeIndices[i] * 3], cubeVertices[cubeIndices[i] * 3 + 1], cubeVertices[cubeIndices[i] * 3 + 2]);
+        }
+        glEnd();
     }
 
     // Method to set a new position for the block
